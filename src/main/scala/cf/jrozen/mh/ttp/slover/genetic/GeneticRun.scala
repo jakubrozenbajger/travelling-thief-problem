9406ad845dd834b76bfb190a4fef09ac889b4e9d
Jakub Rozenbajger <thequetran@gmail.com>
2018-11-30 09:27:27 +0100

    uptd

--- src/main/scala/cf/jrozen/mh/ttp/slover/genetic/GeneticRun.scala
+++ src/main/scala/cf/jrozen/mh/ttp/slover/genetic/GeneticRun.scala
@@ -15,18 +15,18 @@ import scala.util.Random
 object GeneticRun extends App {
 
   System.out.println(Banner.title)
-  val params = GeneticParameters(populationSize = 130, noOfGenerations = 400, tournamentSize = 5, mutationProbability = 0.022, crossoverProbability = 0.119)
+  val params = GeneticParameters(populationSize = 100, noOfGenerations = 200, tournamentSize = 5, mutationProbability = 0.022, crossoverProbability = 0.119)
+//
+//  val problemName = "medium_2"
+//  val stats = solve(problemName, params)
+//  System.out.println("Last stats: " + stats.last)
+//  chart(params).show(stats)
 
-  val problemName = "medium_1"
-  val stats = solve(problemName, params)
-  System.out.println("Last stats: " + stats.last)
-  chart(params).show(stats)
 
-
-//  val problemNames = List("easy_1", "easy_4", "medium_0", "medium_1", "hard_0")
-//  private val result: Map[String, Stats] = problemNames.map(pn => runFor(pn, params)).toMap
-//  println(result)
-//  CsvFormat.format(result).foreach(println)
+  val problemNames = List("easy_1", "easy_4", "medium_0", "medium_1")
+  private val result: Map[String, Stats] = problemNames.map(pn => runFor(pn, params)).toMap
+  println(result)
+  CsvFormat.format(result).foreach(println)
 
 
 
@@ -36,18 +36,18 @@ object GeneticRun extends App {
   private def solve(problemName: String, params: GeneticParameters) = {
     SoftReference
     implicit val context = new Context(Loader.load(problemName))
-    implicit val saParameters = SimulatedAnnealingParameters(iterations = 5000, startingTemperature = 200.0, coolingRate = 0.001, stopTemperature = 0.0000001)
+    implicit val saParameters = SimulatedAnnealingParameters(iterations = 6000, startingTemperature = 200.0, coolingRate = 0.001, stopTemperature = 0.0000001)
     implicit val tabuParameters = TabuParameters(noOfIterations = 800, tabuSize = 120)
     val rnd = new Random()
     import collection.convert.ImplicitConversionsToScala._
-//    val evolutionHistory = Population.initRandom(context, params, new GAMutationStrategy(context, params), EmptyFinish()).runEvolution.asJava().toList
+    val evolutionHistory = Population.initRandom(context, params, new GAMutationStrategy(context, params), EmptyFinish()).runEvolution.asJava().toList
 //    val evolutionHistory = Population.initRandom(context, params, new GAMutationStrategy(context, params), TabuFinish()).runEvolution.asJava().toList
-//    val evolutionHistory = Population.initRandom(context, params, new SAMutationStrategy(new GAMutationStrategy(context, params), rnd.nextFloat() < 0.0010 ), EmptyFinish()).runEvolution.asJava().toList
-    val evolutionHistory = Population.initRandom(context, params, new SAMutationStrategy(new GAMutationStrategy(context, params), rnd.nextFloat() < 0.0070 ), TabuFinish()).runEvolution.asJava().toList
+//    val evolutionHistory = Population.initRandom(context, params, new SAMutationStrategy(new GAMutationStrategy(context, params), rnd.nextFloat() < 0.0091210 ), EmptyFinish()).runEvolution.asJava().toList
+//    val evolutionHistory = Population.initRandom(context, params, new SAMutationStrategy(new GAMutationStrategy(context, params), rnd.nextFloat() < 0.0070 ), TabuFinish()).runEvolution.asJava().toList
     evolutionHistory.map(_.stats)
   }
 
-  def runFor(problemName: String, parameters: GeneticParameters, times: Int = 10): (String, Stats) = {
+  def runFor(problemName: String, parameters: GeneticParameters, times: Int = 3): (String, Stats) = {
 //    val stat: Double  = solve(problemName, params).map(s => s.max()).max
     (problemName, Stats.of(
       (0 until times).map { i =>
